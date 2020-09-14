a5d387cd3246b735bdf348e93f0ee4e54246adf3
k-kudo-hub <tongariisaibou@gmail.com>
2020-09-14 21:09:02 +0900

    自動デプロイの用意（修正1）

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'sup_app'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'k-kudo-hub/sup_app.git'
+set :repo_url,  'git@github.com:k-kudo-hub/sup_app.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
