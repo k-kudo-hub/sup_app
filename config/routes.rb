c43ab0d3f3083d4bca9d2d4a01cccf563df9800b
k-kudo-hub <tongariisaibou@gmail.com>
2020-09-19 17:11:15 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :clients, only: [:new, :create] do
-    resources :details
   end
+  get 'details', to: 'clients#new_detail'
+  post 'details', to: 'clients#create_detail'
+  get 'caregivers', to: 'clients#new_caregiver'
+  post 'caregivers', to: 'clients#create_caregiver'
 
 end
