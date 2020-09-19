9d2fc8fe4683eee626ed073e19dfcce8418d1bec
k-kudo-hub <tongariisaibou@gmail.com>
2020-09-19 17:10:27 +0900

    バリデーションの再設定

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,13 +1,17 @@
 class Client < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :status
 
   has_one :detail
+  has_one :caregiver
   has_one :room
 
   with_options presence: {message: "が空です"} do
     validates :name
-    validates :name_kana,          format:{with:/\A[ァ-ヶー－]+\z/, message:"は全角カタカナのみ登録できます。"}
-    validates :birth,:room_number, format:{with: /\A[0-9]+\z/, message:"は数字のみ登録できます。"}
+    validates :name_kana,   format:{with:/\A[ァ-ヶー－]+\z/, message:"は全角カタカナのみ登録できます。"}
+    validates :birth
+    validates :room_number, format:{with: /\A[0-9]+\z/, message:"は数字のみ登録できます。"}
   end
-  validates :status_id,      numericality:{ other_than: 1 , message: "が選択されていません"}
+    validates :status_id,   numericality:{ other_than: 1 , message: "が選択されていません"}
 
 end
