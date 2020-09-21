7af40f535abdca439e712d78dbe45ee0bcc58915
k-kudo-hub <tongariisaibou@gmail.com>
2020-09-21 13:16:38 +0900

    Detailモデル単体テストコード

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -3,8 +3,8 @@ class Detail < ApplicationRecord
   belongs_to :client, optional: true
 
   with_options presence: {message: "が空です"} do
-    validates :mbp_high
-    validates :mbp_low
+    validates :mbp_high,    numericality: { only_integer: true, message:"は半角数字で入力してください"}
+    validates :mbp_low,     numericality: { only_integer: true, message:"は半角数字で入力してください"}
   end
 
 end
--- spec/models/detail_spec.rb
+++ spec/models/detail_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Detail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @detail = FactoryBot.build(:detail)
+  end
+
+  #bundle exec rspec spec/models/detail_spec.rb
+  
+  describe 'お客様医療情報登録' do
+    context 'お客様医療情報登録がうまくいくとき' do
+      it "mbp_high,mbp_lowが揃っていれば登録できる" do
+        expect(@detail).to be_valid
+      end
+      it "past_historyは空でも登録できる" do
+        @detail.past_history = ""
+        expect(@detail).to be_valid
+      end
+      it "illnessは空でも登録できる" do
+        @detail.illness = ""
+        expect(@detail).to be_valid
+      end
+      it "medicineは空でも登録できる" do
+        @detail.medicine = ""
+        expect(@detail).to be_valid
+      end
+    end
+
+    context 'お客様医療情報登録がうまくいかないとき' do
+      it "mbp_highが空だと登録できない" do
+        @detail.mbp_high = nil
+        @detail.valid?
+        expect(@detail.errors.full_messages).to include("Mbp high が空です")
+      end
+      it "mbp_lowが空だと登録できない" do
+        @detail.mbp_low = nil
+        @detail.valid?
+        expect(@detail.errors.full_messages).to include("Mbp low が空です")
+      end
+      it "mbp_highが数字以外だと登録できない" do
+        @detail.mbp_high = "あああ"
+        @detail.valid?
+        expect(@detail.errors.full_messages).to include("Mbp high は半角数字で入力してください")
+      end
+      it "mbp_lowが空だと登録できない" do
+        @detail.mbp_low = "あああ"
+        @detail.valid?
+        expect(@detail.errors.full_messages).to include("Mbp low は半角数字で入力してください")
+      end
+    end
+
+  end
 end
