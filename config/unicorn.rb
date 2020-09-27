5fa39a0d115e386873045e4c63a7baa95b35a464
k-kudo-hub <tongariisaibou@gmail.com>
2020-09-27 13:05:28 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -7,7 +7,7 @@ ENV["BUNDLE_GEMFILE"] = RAILS_ROOT + "/Gemfile"
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory "#{app_path}/current"  # 「current」を指定
+# working_directory "#{app_path}/current"  # 「current」を指定
 
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"  # 「shared」の中を参照するよう変更
