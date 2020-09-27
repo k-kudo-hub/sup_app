aead38adadfbe46c2f85baf7f1ad9a5f4e0253fd
k-kudo-hub <tongariisaibou@gmail.com>
2020-09-27 15:45:19 +0900

    アップローダーの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,9 +3,9 @@ class Message < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :tag
 
+  mount_uploader :picture, PictureUploader
+
   belongs_to :room 
   belongs_to :user
-
-  
 
 end
