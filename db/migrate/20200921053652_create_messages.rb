a4b362c400d0b8f7c4b9b65699317a9883314b72
k-kudo-hub <tongariisaibou@gmail.com>
2020-09-27 15:44:37 +0900

    pictureカラムの追加

--- db/migrate/20200921053652_create_messages.rb
+++ db/migrate/20200921053652_create_messages.rb
@@ -4,6 +4,7 @@ class CreateMessages < ActiveRecord::Migration[6.0]
       t.integer :room_id,        null: false, foreign_key: true
       t.integer :user_id,        null: false, foreign_key: true
       t.text :content,           null: false
+      t.text :picture
       t.integer :tag_id
       t.timestamps
     end
