c1815f8318f2c3e3f7d1bd3c01e6608b241f27bf
k-kudo-hub <tongariisaibou@gmail.com>
2020-09-27 21:50:12 +0900

    ビューファイルの脚色

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -34,7 +34,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   version :top_user do
-    process resize_to_limit: [90,90]
+    process resize_to_fill: [90,90]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
