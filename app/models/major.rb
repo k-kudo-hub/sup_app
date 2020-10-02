468c57305767679092f30114fee5e4eb1b1f00a6
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-02 21:06:47 +0900

    ActiveHash用モデルの記述

---
+++ app/models/major.rb
@@ -0,0 +1,10 @@
+class Major < ActiveHash::Base
+
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '身体介助' },
+    { id: 3, name: '生活援助' },
+    { id: 4, name: '処置' },
+    { id: 5, name: 'リハビリテーション' },
+  ] 
+end
