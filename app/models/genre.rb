09c0ebdd19ce2d772ebd40bd2994c02fb87fc21c
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-08 20:32:49 +0900

    ActiveHashの記述

---
+++ app/models/genre.rb
@@ -0,0 +1,24 @@
+class Genre < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '--'},
+    { id: 1, name: '骨折' },
+    { id: 2, name: '捻挫・脱臼' },
+    { id: 3, name: '打撲' },
+    { id: 4, name: '切傷・擦過傷' },
+    { id: 5, name: 'やけど' },
+    { id: 6, name: 'その他の外傷' },
+    { id: 7, name: '誤食' },
+    { id: 8, name: '誤嚥・誤飲' },
+    { id: 9, name: '誤薬' },
+    { id: 10, name: '落薬' },
+    { id: 11, name: '食中毒' },
+    { id: 12, name: '感染症' },
+    { id: 13, name: '結核' },
+    { id: 14, name: '疥癬' },
+    { id: 15, name: '自傷行為' },
+    { id: 16, name: '利用者間のトラブル' },
+    { id: 17, name: '無断外出' },
+    { id: 18, name: '暴力行為' },
+    { id: 19, name: 'その他' }
+  ] 
+end
---
+++ app/models/place.rb
@@ -0,0 +1,23 @@
+class Place < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '居室（フロア）' },
+    { id: 3, name: '居室（トイレ）' },
+    { id: 4, name: '居室（キッチン）' },
+    { id: 5, name: '廊下' },
+    { id: 6, name: 'リビング' },
+    { id: 7, name: '共用トイレ' },
+    { id: 8, name: '浴室' },
+    { id: 9, name: '脱衣所' },
+    { id: 10, name: 'エレベーター' },
+    { id: 11, name: '階段' },
+    { id: 12, name: 'エントランス' },
+    { id: 13, name: 'ロビー' },
+    { id: 14, name: 'ベランダ' },
+    { id: 15, name: 'スタッフルーム' },
+    { id: 16, name: 'マシンルーム' },
+    { id: 17, name: '健康管理室' },
+    { id: 18, name: '外出先' },
+    { id: 19, name: 'その他' },
+  ] 
+end
