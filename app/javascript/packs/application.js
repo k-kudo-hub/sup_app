6595a52315af56320c72126d603b6d63efb0b089
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-11 18:21:25 +0900

    ヘッダーメニューを改善

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,7 @@ require('jquery')
 require("../record")
 require("../carryout")
 require("../scroll")
+require("../menu")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
