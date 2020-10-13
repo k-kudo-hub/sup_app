91683c9938745b968ce33f4859393feb6e6b117a
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-13 20:33:27 +0900

    デプロイ修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = 'ws://Elastic IP/cable'
-  config.action_cable.allowed_request_origins = [ 'http://Elastic IP' ]
+  # ActionCable.server.config.disable_request_forgery_protection = true
+  # config.action_cable.url = 'ws://Elastic IP/cable'
+  config.action_cable.allowed_request_origins = [ 'http://54.150.159.17/' ]
 end
