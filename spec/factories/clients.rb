89ed7ee04c58c44934b77699e2371edab6aaec82
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-19 20:30:30 +0900

    Room_userモデルの単体テストコード

--- spec/factories/clients.rb
+++ spec/factories/clients.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     name_kana             {"スズキタロウ"}
     room_number           {444}
     status_id             {2}
+    sex_id                {2}
     birth                 {Faker::Date.between(from: '1920-01-01', to: '2015-12-31')}
   end
 end
