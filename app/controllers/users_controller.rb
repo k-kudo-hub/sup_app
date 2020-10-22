9aa29c58c7b5b06e4ca9e57947ac1d3e5429f58b
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-22 18:22:04 +0900

    テストコードの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController  
 
   def show
-    @user = User.find(params[:id])
+    binding.pry
+    @user = User.find(current_user.id)
     clientsid = @user.relationships.pluck(:client_id)
     @clients = Client.find(clientsid)
     @lists = Room.pluck(:client_id)
