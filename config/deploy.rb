826af992cdae32928ec26aa8a1ab3833830faa28
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-22 21:54:21 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-set :linked_files, %w[config/master.key]
+# set :linked_files, %w[config/master.key]
 
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
