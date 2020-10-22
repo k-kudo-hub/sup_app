e59baa850e251790ebd53c9b5f1fb62c27972216
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-22 18:22:46 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController  
 
   def show
-    binding.pry
     @user = User.find(current_user.id)
     clientsid = @user.relationships.pluck(:client_id)
     @clients = Client.find(clientsid)
