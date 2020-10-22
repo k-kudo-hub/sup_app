abaaf1876bea276852bebee52ea1dbe3f60e6178
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-22 20:35:40 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,8 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['publickey'],
-                                  keys: ['~/.ssh/tongari6824.pem'] 
+# set :ssh_options, auth_methods: ['publickey'],
+#                                   keys: ['~/.ssh/tongari6824.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
