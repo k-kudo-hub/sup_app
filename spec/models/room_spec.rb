fcbf30de8b0fef92754f8e54be8e61e59502a16a
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-25 11:33:55 +0900

    Update room_spec.rb

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -13,15 +13,5 @@ RSpec.describe Room, type: :model do
         expect(@room).to be_valid
       end
     end
-
-    context 'チャットルーム作成がうまくいかないとき' do
-      it "すでにチャットルームがある場合は作成できない" do
-        @room.client_id = 1
-        @room2 = FactoryBot.build(:room)
-        @room2.client_id = 1
-        @room2.valid?
-        expect(@room2.errors.full_messages).to include("Clientを入力してください")
-      end
-    end
   end
 end
