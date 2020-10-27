a17ee2f1ee8c1ee540f83b47baf9f6277c06fbc4
k-kudo-hub <tongariisaibou@gmail.com>
2020-10-27 16:37:06 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module SupApp
     config.load_defaults 6.0
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
