9215739d13f222f0e41051bcd81254aef9bd6019
k-kudo-hub <tongariisaibou@gmail.com>
2021-02-16 09:15:29 +0900

    お客様ページのビジネスロジックの分離

--- app/models/client.rb
+++ app/models/client.rb
@@ -22,4 +22,75 @@ class Client < ApplicationRecord
     validates :sex_id, numericality: { other_than: 1, message: 'が選択されていません。' }
   end
   validates :status_id, numericality: { other_than: 1, message: 'が選択されていません。' }
+
+  def no_image?
+    self.picture.file.nil?
+  end
+
+  def room_present?
+    self.room.present?
+  end
+
+  def important_in_days?(days)
+    self.room.messages.where(created_at: days).where(tag_id: 3).exists?
+  end
+
+  def caution_in_days?(days)
+    self.room.messages.where(created_at: days).where(tag_id: 2).exists?
+  end
+
+  def message_exists?
+    self.room.messages.exists?
+  end
+
+  def latest_message
+    self.room.messages.last.content
+  end
+
+  def latest_message_time
+    self.room.messages.last.created_at
+  end
+
+  def todays_record(day)
+    self.records.where(start_time: day).order("start_time ASC")
+  end
+
+  def today_nutritions
+    self.nutritions.find_by(date: Date.today)
+  end
+
+  def sum_water_amount
+    today_nutritions.six_water_amount + 
+    today_nutritions.morning_water_amount +
+    today_nutritions.ten_water_amount +
+    today_nutritions.lunch_water_amount +
+    today_nutritions.fifty_water_amount +
+    today_nutritions.dinner_water_amount +
+    today_nutritions.twenty_water_amount
+  end
+
+  def morning_main_meal
+    Meal.find_by(id: self.nutritions.find_by(date: Date.today).morning_meal_m_id).name
+  end
+
+  def morning_sub_meal
+    Meal.find_by(id: self.nutritions.find_by(date: Date.today).morning_meal_s_id).name 
+  end
+
+  def lunch_main_meal
+    Meal.find_by(id: self.nutritions.find_by(date: Date.today).lunch_meal_m_id).name
+  end
+
+  def lunch_sub_meal
+    Meal.find_by(id: self.nutritions.find_by(date: Date.today).lunch_meal_s_id).name 
+  end
+
+  def dinner_main_meal
+    Meal.find_by(id: self.nutritions.find_by(date: Date.today).dinner_meal_m_id).name
+  end
+
+  def dinner_sub_meal
+    Meal.find_by(id: self.nutritions.find_by(date: Date.today).dinner_meal_s_id).name 
+  end
+
 end
