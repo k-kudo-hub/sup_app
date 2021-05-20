59055de1e874ba7b62cdc396a35a8e363f3d359a
k-kudo-hub <tongariisaibou@gmail.com>
2021-05-21 08:29:15 +0900

    不要なコメントアウトの削除

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,43 +1,21 @@
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.14.1'
-
-# Capistranoのログの表示に利用する
-set :application, 'sup_app'
-
-# どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:k-kudo-hub/sup_app.git'
-
-# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-
 set :rbenv_type, :user
-set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、’2.6.5’ です
-
-# どの公開鍵を利用してデプロイするか
+set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/tongari6824.pem'] 
-
-# プロセス番号を記載したファイルの場所
+                  keys: ['~/.ssh/ssh_key.pem'] 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-
-# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
-# set :linked_files, %w[config/master.key]
-
-# デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
-
   desc 'upload master.key'
   task :upload do
     on roles(:app) do |_host|
       execute "mkdir -p #{shared_path}/config" if test "[ ! -d #{shared_path}/config ]"
-      # upload!('config/master.key', "#{shared_path}/config/master.key")
     end
   end
   before :starting, 'deploy:upload'
